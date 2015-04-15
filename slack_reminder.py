56608e34dd484b4cc9ed081de8b0daa5068a00b0
David Zhang <davzee@hotmail.com>
2015-04-14 21:16:43 -0400

    Clean-up code

--- slack_reminder.py
+++ slack_reminder.py
@@ -1,63 +1,75 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+import json
 import requests
-import getpass
 
-BASE = 'https://api.github.com/repos/Eventmobi/'
+BASE = 'https://api.github.com/repos/YOUR_ORGANIZATION/'
 PROJECTS = {
-    'ansible': '#platform',
-    'Flux': '#platform',
-    'analytics_engine': '#front-end',
-    'contentmanager': '#front-end',
-    'fusion-realtime': '#front-end',
-    'messaging': '#front-end',
-    'mobileapp': '#front-end',
-    'web-applications': '#front-end',
-    'cms-facade': '#front-end',
+    #github_repo_name : slack_channel_name
 }
 CHANNEL_MAP = {
-    'davidozhang': "@davidzhang",
+    #github_user_name : slack_user_name
 }
-REPO_URLS = {project: BASE + project + '/pulls' for project in PROJECTS}
+SLACK_URL = ('https://hooks.slack.com/services/123456/789012345/'
+             '67890123456')
+NOTIFICATION = '<{0}|Click here> to review {1} PR with title: {2}'
 
+class SlackReminder(object):
 
-def map_url(project, number):
-    return 'https://github.com/EventMobi/{0}/pull/{1}'.format(project, number)
+    def __init__(self, oauth_key):
+        self.payloads = []
+        self.projects = PROJECTS
+        self.channels = CHANNEL_MAP
+        self.repos = {p: BASE + p + '/pulls' for p in self.projects}
 
+        self._pulls(oauth_key)
 
-def pulls(auth):
-    text, assignee = None, None
-    headers = {'Authorization': 'Bearer {0}'.format(auth)}
-    for project, url in REPO_URLS.iteritems():
-        try:
+    def _map_url(self, project, number):
+        return 'https://github.com/YOUR_ORGANIZATION/{0}/pull/{1}'.format(
+            project,
+            number,
+        )
+
+    def _pulls(self, auth):
+        headers = {'Authorization': 'Bearer {0}'.format(auth)}
+        for project, url in self.repos.items():
             r = requests.get(url, headers=headers)
+            if r.status_code == 401:
+                raise Exception('Invalid OAuth Token')
             data = r.json()
-            if len(data) > 0:
-                for i in range(len(data)):
-                    text = ''.join('<' + map_url(project, data[i]['number']) + '|Click here> to review PR in {0} with title: {1}'.format(project, data[i]['title']))
-                    if data[i]['assignee']:
-                        assignee = CHANNEL_MAP[data[i]['assignee']['login']]
-                    else:
-                        assignee = PROJECTS[project]
-                    if text is not None and assignee is not None:
-                        post(text, assignee)
-        except ValueError:
-            pass
+            if data is not None and len(data) > 0:
+                self._process(data, project)
 
+    def _process(self, data, project):
+        for d in data:
+            if d['assignee']:
+                assignee_data = d['assignee']
+                assignee = self.channels[assignee_data['login']]
+            else:
+                assignee = self.projects[project]
+            self._post(
+                assignee,
+                project=project,
+                title=d['title'],
+                number=d['number'],
+            )
 
-def post(text, username):
-    # find your token https://api.slack.com/web
-    payload = """{
-        "access_token":"xoxp-2162427386-3360743925-3869996840-aa2c89",
-        "text":"%s",
-        "username":"slack-pr-reminder-bot",
-        "channel": "%s"
-    }""" % (text, username)
-    r = requests.post(
-        'https://hooks.slack.com/services/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',
-        payload,
-    )
-    print r.text
+    def _post(self, assignee, project, title, number):
+        text = ''.join(
+            NOTIFICATION.format(
+                self._map_url(project, number),
+                project,
+                title,
+            ),
+        )
+        # find your token https://api.slack.com/web
+        payload = {
+            'access_token': 'YOUR_ACCESS_TOKEN',
+            'text': text,
+            'username': 'slack-pr-reminder-bot',
+            'channel': assignee,
+        }
+        r = requests.post(SLACK_URL, json.dumps(payload))
 
 if __name__ == "__main__":
-    print pulls(getpass.getpass('Enter your Github OAUTH Key: '))
+    s = SlackReminder(getpass.getpass('Enter your Github OAUTH Key: '))
